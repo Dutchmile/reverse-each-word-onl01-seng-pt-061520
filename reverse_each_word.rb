222ed1dcb7bed51a5f99a155aa348f7efd85a739
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 19:48:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,13 @@
 
 def reverse_each_word(sentence1)
 sentence1_reversed = []
-sentence1.join(" ")
+sentence1.split(" ")
   sentence1.each do |word|
     sentence1_reversed.push(word.reverse)
   end
   sentence1_reversed
 end
+
+def reverse_each_word(sentence2)
+end
+  
