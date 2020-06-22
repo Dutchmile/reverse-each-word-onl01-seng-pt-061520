f2334dcb9b5735b78f426422e69502fecb711084
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 19:40:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(string)
 string_reversed = []
-string.join(" ")
+string_array = string.join(" ")
   string.each do |word|
     string_reversed.push(word.reverse)
   end
-  puts string_reversed
+  string_reversed
 end
