0418c9cabd0c9441d3c373b3bbb2e940320f24ca
Christopher Dutcher <cadutcher@icloud.com>
2020-06-22 20:07:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
 
 
-def reverse_each_word(sentence1, sentence2)
-sentence1_reversed = []
+def reverse_each_word(sentence2)
+# sentence1_reversed = []
 sentence2_reversed = []
-array_sentence1 = sentence1.split(" ")
+# array_sentence1 = sentence1.split(" ")
 array_sentence2 = sentence2.split(" ")
-  array_sentence1.each do |word|
-    sentence1_reversed.push(word.reverse)
-  end
+  # array_sentence1.each do |word|
+    # sentence1_reversed.push(word.reverse)
+  # end
   array.sentence2.collect do |word|
     sentence2_reversed << word.reverse
   end
   sentence2_reversed.join(" ")
-  return sentence1_reversed.join(" ")
+  # return sentence1_reversed.join(" ")
 end
 
 # def reverse_each_word(sentence2)
